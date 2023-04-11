ea8704119cff9098320e1f55d6f276550673588d
Aakash SL <slaakash06@gmail.com>
2023-04-11 12:07:57 +0530

    backend changes

--- alertme_backend/app.py
+++ alertme_backend/app.py
@@ -5,13 +5,11 @@ app = Flask(__name__)
 
 @app.route('/detect-spam-sms', methods=['POST'])
 def detect_spam_sms():
-    
     headers = request.json['headers']
     result = is_spam_sms(headers)
     return jsonify({'is_spam': result})
 
 def is_spam_sms(headers):
-    
     # Check for common spam keywords in the headers
     keywords = ['win', 'free', 'prize', 'offer', 'congratulations']
     for keyword in keywords:
@@ -35,22 +33,9 @@ def is_spam_sms(headers):
     if recipients:
         recipients = recipients.group(1).strip()
         if ',' in recipients or ';' in recipients:
-            
-            return "This SMS is Spam!"
-       
-           
+            return True
     
-    return "This SMS is Not Spam"
-
-@app.after_request
-def after_request_func(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Credentials', True)
-    response.headers.add(
-        'Access-Control-Allow-Headers',
-        'Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale')
-    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    return response
+    return False
 
 if __name__ == '__main__':
     app.run(debug=True)
